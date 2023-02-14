f99f2153b9919bc65083aa0861d5229242b83c67
BulbaYim <daniil_bandarenka2@epam.com>
2023-02-15 00:26:27 +0300

    hm_01 second remark change

--- hw_01/hw_1.py
+++ hw_01/hw_1.py
@@ -7,14 +7,14 @@ def main():
     print(f'Created array:\n{array_random}\n')
 
     # TASK 2: sort list from min to max (without using sort())
-    for i in range(len(array_random)):
-        min = i # index of start min element
-        for j in range(i + 1, len(array_random)):
+    for elem in range(len(array_random)):
+        min = elem # index of start min element
+        for j in range(elem + 1, len(array_random)):
             # select index of the smallest element by comparing j element with current min element
             if array_random[j] < array_random[min]:
                 min = j
             # swap i element with the min element
-        array_random[min], array_random[i] = array_random[i], array_random[min]
+        array_random[min], array_random[elem] = array_random[elem], array_random[min]
 
     print(f'Sorted array:\n{array_random}\n')
 
@@ -23,13 +23,14 @@ def main():
     '''
     #use comparing remainder of division 
     odd_array = [i for i in array_random if i % 2]
-    even_array = [i for i in array_random if i % 2 == 0]
+    even_array = [i for i in array_random if not i % 2 ]
 
     # use sum() and len() functions to find average value of array
     avg_odd = round(sum(odd_array) / len(odd_array), 2)
     avg_even = round(sum(even_array) / len(even_array), 2)
 
     print(f'The average of odd: {avg_odd}\nThe average of even: {avg_even}\n')
+
 
 if __name__ == "__main__":
     main()
