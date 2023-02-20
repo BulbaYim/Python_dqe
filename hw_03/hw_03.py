7256e9497a406db31df87f3e7ad5165e78a14659
BulbaYim <daniil_bandarenka2@epam.com>
2023-02-20 16:03:36 +0100

    Changes remark hw_03

--- hw_03/hw_03.py
+++ hw_03/hw_03.py
@@ -25,9 +25,12 @@ normalized_sentences = []
 last_words = []
 
 for row in text_rows:
-	if row in ('', ' ') : continue # go to next element of array, if current is empty
+	if row in ('', ' ') : 
+		continue # go to next element of array, if current is empty
+	
 	# delete '\t', start/end whitespaces, make lower case
 	row = row.replace('\t', '').strip().lower()	
+	
 	# split row in sentences array, add '.', delete start/end whitespaces
 	sentences = [
 		sentence + '.' if sentence[-1] != ':' else sentence 
@@ -37,15 +40,19 @@ for row in text_rows:
 	
 	for sentence in sentences:
 		sentence = sentence.strip().capitalize()
+		
 		# change 'iz' on 'is'
 		sentence = re.sub("\s{1}iz\s{1}", ' is ',  sentence, re.IGNORECASE)
+		
 		# find the last word of the sentence and add it to last_words array
 		for match in re.finditer("\s{1}[A-Za-z0-9_-]+[.]$", sentence, re.IGNORECASE):
-			last_words.append(sentence[match.start() + 1: match.end() - 1])
+			last_word = sentence[match.start() + 1: match.end() - 1]
+			last_words.append(last_word)
 		normalized_sentence +=  ' ' + sentence
+
 	normalized_sentences.append(normalized_sentence.strip())
  
-last_sentence = ' '.join(word for word in last_words)
+last_sentence = ' '.join(last_words)
 normalized_sentences.append(last_sentence.capitalize() + '.')
 finall_text = '\n'.join(sentence for sentence in normalized_sentences)
 print(finall_text)
